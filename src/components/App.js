11423fa79060dc978fc2c7f7f6de587d1b330bf8
Vivek Singh <vivekonline01@gmail.com>
2018-08-22 04:55:11 +0530

    Note comonent created

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import uuid from 'node-uuid';
+import Note from './Note';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes : [
+        {
+          key: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          key: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          key: uuid.v4(),
+          task: 'Do Laundry'
+        } 
+      ]
+    }
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        key: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
+  };
+
+
   render() {
-    const notes = [
-      {
-        key: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        key: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        key: uuid.v4(),
-        task: 'Do Laundry'
-      } 
-    ];
+    const notes = this.state.notes; 
     return (
       <div>
+        <button onClick={this.addNote}>+</button>  
         <ul>
-          {notes.map(note => <li id={note.key}>{note.task}</li>)}
+          {notes.map(note => <li key={note.key}><Note task={note.task}/></li>)}
         </ul>
       </div>
     );
   }
+
+  
 }
